a8cf0d36e96382d39e5851734783d716223bbf05
Murat Şahin <92647890+mrtSahin@users.noreply.github.com>
2023-04-05 02:30:19 +0300

    first commit

---
+++ grafik/grafik/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace grafik
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBoxEgitim = new System.Windows.Forms.TextBox();
+            this.textBoxGida = new System.Windows.Forms.TextBox();
+            this.textBoxAkaryakit = new System.Windows.Forms.TextBox();
+            this.textBoxSaglik = new System.Windows.Forms.TextBox();
+            this.textBoxDiger = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(305, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aylık Kredi Kartı Harcamaları";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(68, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Eğitim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(68, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gıda";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(68, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Akaryakıt";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(68, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sağlık";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(68, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Diğer";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(81, 459);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(214, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Seçilen Harcama Yüzdesi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Yüzde Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(540, 81);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Legend = "Legend1";
+            series4.Name = "s1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // textBoxEgitim
+            // 
+            this.textBoxEgitim.Location = new System.Drawing.Point(195, 81);
+            this.textBoxEgitim.Name = "textBoxEgitim";
+            this.textBoxEgitim.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEgitim.TabIndex = 10;
+            // 
+            // textBoxGida
+            // 
+            this.textBoxGida.Location = new System.Drawing.Point(195, 127);
+            this.textBoxGida.Name = "textBoxGida";
+            this.textBoxGida.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGida.TabIndex = 11;
+            // 
+            // textBoxAkaryakit
+            // 
+            this.textBoxAkaryakit.Location = new System.Drawing.Point(195, 178);
+            this.textBoxAkaryakit.Name = "textBoxAkaryakit";
+            this.textBoxAkaryakit.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAkaryakit.TabIndex = 12;
+            // 
+            // textBoxSaglik
+            // 
+            this.textBoxSaglik.Location = new System.Drawing.Point(195, 229);
+            this.textBoxSaglik.Name = "textBoxSaglik";
+            this.textBoxSaglik.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSaglik.TabIndex = 13;
+            // 
+            // textBoxDiger
+            // 
+            this.textBoxDiger.Location = new System.Drawing.Point(195, 286);
+            this.textBoxDiger.Name = "textBoxDiger";
+            this.textBoxDiger.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDiger.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Eğitim",
+            "Gıda",
+            "Akaryakıt",
+            "Sağlık",
+            "Diğer"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 408);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 528);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxDiger);
+            this.Controls.Add(this.textBoxSaglik);
+            this.Controls.Add(this.textBoxAkaryakit);
+            this.Controls.Add(this.textBoxGida);
+            this.Controls.Add(this.textBoxEgitim);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "2020123019 Murat Şahin";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBoxEgitim;
+        private System.Windows.Forms.TextBox textBoxGida;
+        private System.Windows.Forms.TextBox textBoxAkaryakit;
+        private System.Windows.Forms.TextBox textBoxSaglik;
+        private System.Windows.Forms.TextBox textBoxDiger;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ grafik/grafik/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace grafik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //  HESAPLA BUTONU
+        {
+
+            chart1.Series["s1"].Points.Clear();
+
+            var degerler = degerlerDon();
+            string[] degerlerText = { "Egitim", "Gida", "Akaryakit", "Saglik", "Diger"};
+
+            for (int i = 0; i < 5; i++)
+            {
+                  chart1.Series["s1"].Points.AddXY(oranBul(degerler[i], degerler[5]) + "\n" + degerlerText[i], (degerler[i] * 100 / degerler[5]).ToString());
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // YÜZDE HESAPLA BUTONU
+        {
+            var degerler = degerlerDon();
+            var istenenAlanIndex = comboBox1.SelectedIndex;
+            var istenenAlanText = comboBox1.Text;
+            var oran = oranBul(degerler[istenenAlanIndex], degerler[5]);
+            label7.Text= istenenAlanText+" harcamaları "+oran+" oranındadır";
+
+        }
+
+        private int[] degerlerDon()
+        {
+            int egitim = Convert.ToInt32(textBoxEgitim.Text);
+            int gida = Convert.ToInt32(textBoxGida.Text);
+            int akaryakit = Convert.ToInt32(textBoxAkaryakit.Text);
+            int saglik = Convert.ToInt32(textBoxSaglik.Text);
+            int diger = Convert.ToInt32(textBoxDiger.Text);
+
+            int toplam = egitim + gida + akaryakit + saglik + diger;
+
+            int[] degerler = { egitim ,gida, akaryakit, saglik , diger, toplam };
+            return degerler;
+
+        }
+
+        private string oranBul(int deger,int toplam)
+        {
+            return "%"+Math.Round((double)deger * 100 / toplam, 1);
+        }
+    }
+}
