9b3d7c58f81d2722893c7105845d0578d012dbda
Murat Şahin <92647890+mrtSahin@users.noreply.github.com>
2023-04-05 02:29:45 +0300

    siralamaAlgoritmasi

---
+++ siralama algoritmasi/siralama algoritmasi/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace siralama_algoritmasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dizi_elemanlari = new System.Windows.Forms.ListBox();
+            this.siralanmis_dizi = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "KB Sıralama";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(115, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "BK Sıralama";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dizi_elemanlari
+            // 
+            this.dizi_elemanlari.FormattingEnabled = true;
+            this.dizi_elemanlari.Location = new System.Drawing.Point(361, 44);
+            this.dizi_elemanlari.Name = "dizi_elemanlari";
+            this.dizi_elemanlari.Size = new System.Drawing.Size(185, 95);
+            this.dizi_elemanlari.TabIndex = 4;
+            // 
+            // siralanmis_dizi
+            // 
+            this.siralanmis_dizi.FormattingEnabled = true;
+            this.siralanmis_dizi.Location = new System.Drawing.Point(361, 178);
+            this.siralanmis_dizi.Name = "siralanmis_dizi";
+            this.siralanmis_dizi.Size = new System.Drawing.Size(185, 95);
+            this.siralanmis_dizi.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.siralanmis_dizi);
+            this.Controls.Add(this.dizi_elemanlari);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Murat Şahin 2020123019";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox dizi_elemanlari;
+        private System.Windows.Forms.ListBox siralanmis_dizi;
+    }
+}
+
---
+++ siralama algoritmasi/siralama algoritmasi/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace siralama_algoritmasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            int girdi=Convert.ToInt16(textBox1.Text); 
+            dizi_elemanlari.Items.Add(girdi);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)  
+        {
+            siralanmis_dizi.Items.Clear(); 
+            int[] dizi = new int[dizi_elemanlari.Items.Count];
+            for (int i = 0; i < dizi_elemanlari.Items.Count; i++)
+            {
+                dizi[i] = Convert.ToInt32(dizi_elemanlari.Items[i]);
+            }
+
+            int[] siralanmisDizi = Insertion(dizi);
+            foreach (int p in siralanmisDizi)
+            {
+                siralanmis_dizi.Items.Add(p);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            siralanmis_dizi.Items.Clear();
+
+            int[] dizi = new int[dizi_elemanlari.Items.Count];
+            for (int i = 0; i < dizi_elemanlari.Items.Count; i++)
+            {
+                dizi[i] = Convert.ToInt32(dizi_elemanlari.Items[i]);
+            }
+
+            int[] siralanmisDizi=Insertion(dizi);
+            
+
+            for (int i = siralanmisDizi.Length-1; i >= 0; i--)
+            {
+                siralanmis_dizi.Items.Add(siralanmisDizi[i]);
+            }
+        }
+
+        public int[] Insertion(int[] dizi)
+        {
+            for (int i = 0; i < dizi.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (dizi[j - 1] > dizi[j])
+                    {
+                        int temp = dizi[j - 1];
+                        dizi[j - 1] = dizi[j];
+                        dizi[j] = temp;
+                    }
+                }
+            }
+            return dizi;
+        }
+    }
+}
+
+
+// BUBBLE SORT
+/*
+ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace siralama_algoritmasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // dizi elemanlarını ekleme
+        {
+            try
+            {
+                int sayi=Convert.ToInt32(textBox1.Text); 
+                dizi_elemanlari.Items.Add(sayi);
+            }catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // küçükten büyüğe sıralama
+        {
+            siralanmis_dizi.Items.Clear(); 
+            int[] dizi = new int[dizi_elemanlari.Items.Count];
+            for (int i = 0; i < dizi_elemanlari.Items.Count; i++)
+            {
+                dizi[i] = Convert.ToInt32(dizi_elemanlari.Items[i]);
+            }
+            
+            int temp;
+            for (int j = 0; j <= dizi.Length - 2; j++)
+            {
+                for (int i = 0; i <= dizi.Length - 2; i++)
+                {
+                    if (dizi[i] > dizi[i + 1])
+                    {
+                        temp = dizi[i + 1];
+                        dizi[i + 1] = dizi[i];
+                        dizi[i] = temp;
+                    }
+                }
+            }
+            foreach (int p in dizi)
+            {
+                siralanmis_dizi.Items.Add(p);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)// büyükten küçüğe sıralama
+        {
+            siralanmis_dizi.Items.Clear();
+
+            int[] dizi = new int[dizi_elemanlari.Items.Count];
+            for (int i = 0; i < dizi_elemanlari.Items.Count; i++)
+            {
+                dizi[i] = Convert.ToInt32(dizi_elemanlari.Items[i]);
+            }
+            int temp;
+            for (int j = 0; j <= dizi.Length - 2; j++)
+            {
+                for (int i = 0; i <= dizi.Length - 2; i++)
+                {
+                    if (dizi[i] > dizi[i + 1])
+                    {
+                        temp = dizi[i + 1];
+                        dizi[i + 1] = dizi[i];
+                        dizi[i] = temp;
+                    }
+                }
+            }
+
+            for (int i = dizi.Length-1; i > 0; i--)
+            {
+                siralanmis_dizi.Items.Add(dizi[i]);
+            }
+        }
+    }
+}
+
+ */
